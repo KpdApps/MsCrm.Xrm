d5f763f1e0246a9c085166c2485635b90ff3dd1f
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2019-01-21 17:20:33 +0300

    Add PluginActionBehavior attribute

---
+++ KpdApps.MsCrm.Xrm/Plugins/PluginActionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KpdApps.MsCrm.Xrm.Plugins
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public class PluginActionBehavior : Attribute
+    {
+        public PluginTargetType TargetType { get; private set; }
+        public string EntityName { get; private set; }
+        public string TargetEntityReferenceName { get; private set; }
+        public string MessageName { get; private set; }
+        public string[] PreImages { get; private set; }
+        public string[] PostImages { get; private set; }
+        public Dictionary<string, Type> InputParameters { get; private set; }
+
+        public PluginActionBehavior(PluginTargetType targetType, string messageName, string entityName = null, string targetEntityReferenceName = null,
+                                        string[] preImages = null, string[] postImages = null, Dictionary<string, Type> inputParameters = null)
+        {
+            TargetType = targetType;
+            MessageName = messageName;
+            EntityName = entityName;
+            TargetEntityReferenceName = targetEntityReferenceName;
+            PreImages = preImages;
+            PostImages = postImages;
+            InputParameters = inputParameters;
+        }
+    }
+}

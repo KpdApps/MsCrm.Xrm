51837dc2eaca33add3a64d6a9e692a76aff1985a
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2019-01-23 13:06:28 +0300

    Add UnitTests for PluginActionBehavior

---
+++ KpdApps.MsCrm.Xrm.UnitTests/Plugins/Behaviors/PluginActionOnCreateBehavior.cs
@@ -0,0 +1,18 @@
+﻿using KpdApps.MsCrm.Xrm.Plugins;
+
+namespace KpdApps.MsCrm.Xrm.UnitTests.Plugins.Behaviors
+{
+    [PluginActionBehavior(PluginTargetType.TargetEntity, "Create", "kpd_entity")]
+    public class PluginActionOnCreateBehavior : PluginActionBase
+    {
+        public PluginActionOnCreateBehavior(PluginState state) : base(state)
+        {
+
+        }
+
+        public override void Execute()
+        {
+
+        }
+    }
+}
